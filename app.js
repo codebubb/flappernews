fb4b9bd2d3e70d1a5b9c570c49337c37bb8f3eca
James Bubb <jpbubb82@gmail.com>
2016-06-22 10:17:25 +0100

    Add complex post object and repeating view element

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var app = angular.module('flappernews', []);
 app.controller('MainCtrl', ['$scope', function($scope){
   $scope.test = 'Hello World';
+  $scope.posts = [
+    {title: 'post1', upvotes: 35},
+    {title: 'post2', upvotes: 2},
+    {title: 'post3', upvotes: 44},
+    {title: 'post4', upvotes: 12},
+    {title: 'post5', upvotes: 7}
+
+  ];
 }]);
