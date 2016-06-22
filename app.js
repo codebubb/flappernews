27dbbcad4523491dd6ae3a711ad2b7d88da4eb72
James Bubb <jpbubb82@gmail.com>
2016-06-22 10:46:47 +0100

    Add posts service

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var app = angular.module('flappernews', []);
-app.controller('MainCtrl', ['$scope', function($scope){
+
+app.factory('posts', [function(){
+  var data = {
+    posts: []
+  };
+  return data;
+}]);
+
+
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
   $scope.test = 'Hello World';
-  $scope.posts = [
-    {title: 'post1', upvotes: 35},
-    {title: 'post2', upvotes: 2},
-    {title: 'post3', upvotes: 44},
-    {title: 'post4', upvotes: 12},
-    {title: 'post5', upvotes: 7}
-  ];
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
     if($scope.title){
       $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
