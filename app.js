c714821d54cbcc5c8d200ac072515b71204eaba6
James Bubb <jpbubb82@gmail.com>
2016-06-22 10:28:40 +0100

    Add linking and upvoting

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.controller('MainCtrl', ['$scope', function($scope){
     {title: 'post3', upvotes: 44},
     {title: 'post4', upvotes: 12},
     {title: 'post5', upvotes: 7}
-
   ];
+  $scope.addPost = function(){
+    if($scope.title){
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.title = '';
+      $scope.link = '';
+    }
+  };
+  $scope.upVote = function(post){
+    post.upvotes +=1;
+  };
 }]);
