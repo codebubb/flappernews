e82b27d71220efcac012aaf52238fdc859ee6f74
James Bubb <jpbubb82@gmail.com>
2016-09-26 12:32:11 +0000

    Add some comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,10 @@
+// Load mongoose library
 var mongoose = require('mongoose');
-
+// Create new schema
 var CommentSchema = new mongoose.Schema({
-  body: String,
-  author: String,
-  upvotes: { type: Number, default: 0},
+  body: String, // The text of the comment
+  author: String, // The person who wrote it
+  upvotes: { type: Number, default: 0}, // Keep track of the upvotes
   post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
 });
 
